d21f38f717adaa9809b8032ad2d2f4ed48318897
g-bolsoni <giovanebolsoni2@gmail.com>
2024-03-14 23:30:03 -0300

    Add usestate

--- App.tsx
+++ App.tsx
@@ -1,24 +1,33 @@
-import { StyleSheet, Image, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Image, View, TouchableOpacity} from 'react-native';
 
 import symbolOn from './assets/pictures/symbol-on.png';
 import symbolOff from './assets/pictures/symbol-off.png';
 
 export default function App() {
-  const isActive = false;
+  const [isActive, setIsActive] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={isActive ? symbolOn : symbolOff}
-      />
-    </View>
+    <TouchableOpacity style={isActive ? styles.containerOn : styles.containerOff} onPress={() => setIsActive(!isActive)}>
+      <View >
+        <Image
+          source={isActive ? symbolOn : symbolOff}
+        />
+      </View>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
     backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerOff: {
+    flex: 1,
+    backgroundColor: '#6a6a6a',
     alignItems: 'center',
     justifyContent: 'center',
   },
